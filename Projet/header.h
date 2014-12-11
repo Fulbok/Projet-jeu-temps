dc76657aa9988b2533c8f0a1bdd48991db70e6ee
Fulbok <xavtls.durand@wanadoo.fr>
2014-12-11 17:10:07 +0100

    Début des fonctions d'ouverture.

--- Projet/header.h
+++ Projet/header.h
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 
-
+//Menu
 int thread_menu();
 void Nvlle_partie();
 int recup_chemin(int nb);
@@ -12,6 +13,9 @@ void pause();
 void resume();
 void quitter ();
 
+
+//Ouvreur
+void recup_chemin(int nb);
 
 
 
--- Projet/ouvreur.c
+++ Projet/ouvreur.c
@@ -1,1 +1,53 @@
 #include "header.h"
+
+
+void recup_chemin(int nb)
+{
+// nb représente la fonction suivante à lancer (enregistrer ou ouvrir)
+if (nb!=1 || nb!=2)
+{
+printf("Erreur appel fonction de récupréation du chemin, mauvais argument");
+exit();
+}
+char chemin;
+
+printf("Veuillez saisir l'adresse exacte du fichier à ouvrir : \n");
+scanf("%s",chemin);
+getchar();
+
+if (nb==1)
+{
+charger(chemin);
+}
+else if(nb==2)
+{
+historique(chemin);
+}
+}
+
+void charger(char chemin)
+{
+
+FILE * fic=NULL;
+fic=fopen(chemin,"r");
+char ligne[100];
+if(fic!=NULL)
+{
+}
+
+
+}
+
+void historique(char chemin)
+{
+
+FILE * fic=NULL;
+fic=fopen(chemin,"r");
+char ligne[100];
+if(fic!=NULL)
+{
+}
+
+
+}
+
