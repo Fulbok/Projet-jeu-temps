255c0b483e993052b8b5000d70162e8b85e1a7a7
AlexTN78 <alexandre.tran78@gmail.com>
2014-12-11 17:07:18 +0100

    affichage menu

--- Projet/header.h
+++ Projet/header.h
@@ -5,6 +5,12 @@
 
 
 int thread_menu();
+void Nvlle_partie();
+int recup_chemin(int nb);
+void interrupt();
+void pause();
+void resume();
+void quitter ();
 
 
 
--- Projet/menu.c
+++ Projet/menu.c
@@ -2,15 +2,60 @@
 
 int thread_menu()
     {
+    int choix,error=1;
     while(1)
         {
-        printf("fudvbaiubviabviadbgdigbadiofgbia");
+
         system("clear");
+
+        printf("\t Menu du jeu : %d\n",error);
+        printf("1. Nouvelle partie\n");
+        printf("2. Charger une partie\n");
+        printf("3. Interrompre la partie & Sauvegarder\n");
+        printf("4. Pause\n");
+        printf("5. Reprendre la partie\n");
+        printf("6. Historique\n");
+        printf("7. Quitter l'application\n\n");
+
+        if(error==0)
+        {
+        printf("\n\nTu ne sais pas lire abruti ?...\n\n");
+        error=1;
+        }
+
+        printf("Votre choix:");
+        scanf("%d", &choix);
+        getchar();
+
+        switch(choix)
+            {
+            case 1 : Nvlle_partie();break;
+            case 2 : recup_chemin(1);break;
+            case 3 : interrupt();break;
+            case 4 : pause();break;
+            case 5 : resume();break;
+            case 6 : recup_chemin(2);break;
+            case 7 : quitter();break;
+            default :error=0;
+            }
+
+
         }
 
 
 
 
 
-
     }
+void Nvlle_partie(){}
+int recup_chemin(int nb){
+return 0;
+}
+void interrupt(){}
+void pause(){}
+void resume(){}
+void quitter (){
+exit(1);
+}
+
+
